1641bfeb805fa7ee0e0b817b835919795c61f93b
ericdourado <ericdourado1@hotmail.com>
2023-01-28 13:52:37 -0300

    implementando rota post e get

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Serie;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class SeriesController extends Controller
+{
+    public function index()
+    {
+        $series = Serie::query()->orderBy('nome')->get();
+
+        return view('series.index')->with('series', $series);
+    }
+
+    public function create()
+    {
+        return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        Serie::create($request->all());
+        return to_route('series.index');
+    }
+}
---
+++ resources/views/series/index.blade.php
@@ -0,0 +1,9 @@
+<x-layout title="Séries">
+    <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
+    
+    <ul class="list-group">
+        @foreach ($series as $serie)
+        <li class="list-group-item">{{ $serie->nome }}</li>
+        @endforeach
+    </ul>
+</x-layout>
